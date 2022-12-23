c0546f5502dc90b1b93f0a85e6b8e55264d1e4d5
Danylo Vovk <113827280+amrawolf@users.noreply.github.com>
2022-12-24 00:21:34 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 #Україна україна чел чел чел чел мда ех ех ех
 
-from collections import Counter
-
 #Алфавітний порядок
 
 def sort_words(input):
@@ -10,38 +8,48 @@ def sort_words(input):
     text.sort()
     text = [w[len(w)//2:] for w in text]
     return ' '.join(text)
+while True:
+    print('a: Сортувати в алфавітному порядку \nb: Топ5 слів \nc: Найдовші слова \nВиберіть операцію: ')
+    choose = input()
 
-print('a: Сортувати в алфавітному порядку \nb: Топ5 слів \nc: Найдовші слова \nВиберіть операцію: ')
-choose = input()
+    print("Text: ")
+    text = input()
 
-print("Text: ")
-text = input()
-
-if choose =='a':
-    print(sort_words(text))
+    if choose =='a':
+        print(sort_words(text))
 
 #Top5
 
-if choose == "b":
-    lst_no = ['.', ',', ':', '!', '"', "'", '[', ']', '-', '—', '(', ')']
-    lst = []
+    elif choose == "b":
+        lst_no = ['.', ',', ':', '!', '"', "'", '[', ']', '-', '—', '(', ')']
+        lst = []
 
-    for text in text.lower().split():
-        if not text in lst_no:
-            _word = text
-            if text[-1] in lst_no:
-                _word = _word[:-1]
-            if text[0] in lst_no:
-                _word = _word[1:]
-            lst.append(_word)
-    _dict = dict()
-    for text in lst:
-        _dict[text] = _dict.get(text, 0) + 1
+        for text in text.lower().split():
+            if not text in lst_no:
+                _word = text
+                if text[-1] in lst_no:
+                    _word = _word[:-1]
+                if text[0] in lst_no:
+                    _word = _word[1:]
+                lst.append(_word)
+        _dict = dict()
+        for text in lst:
+            _dict[text] = _dict.get(text, 0) + 1
 
-    _list = []
-    for key, value in _dict.items():
-        _list.append((value, key))
-        _list.sort(reverse=True)
+        _list = []
+        for key, value in _dict.items():
+            _list.append((value, key))
+            _list.sort(reverse=True)
 
-    for freq, text in _list[0:5]:
-        print(f'{text:>10} -> {freq:>3}')
+        for freq, text in _list[0:5]:
+            print(f'{text:>10} -> {freq:>3}')
+
+#Найдовше слово
+
+    elif choose == 'c':
+        d = text.lower
+        l = text.split()
+        q = ""
+        for i in sorted(l, key=lambda b: len(b), reverse=True):
+            q = q + " " + i
+        print(q[0:9])
